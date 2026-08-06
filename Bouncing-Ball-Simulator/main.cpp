0aedd4183dd28d965e23c60792b033610d38f1d4
KyleKinsella <t54420027@gmail.com>
2026-08-06 15:34:53 +0100

    back working on the project ;) I now have a vertex buffer and an index buffer into my vertex buffer

--- Bouncing-Ball-Simulator/main.cpp
+++ Bouncing-Ball-Simulator/main.cpp
@@ -1,13 +1,9 @@
-//~ #include <iostream>
-
-//~ int main() {
-	//~ std::cout << "This is my Bouncy Ball Simulator." << std::endl;
-//~ }
-
+#include "glad/include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
+#include <iostream>
+
+int main(void) {
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -15,23 +11,54 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    window = glfwCreateWindow(640, 480, "Bouncing Ball Simulator", NULL, NULL);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD\n";
+		return -1;
+	}
+    
+    float positions[] {
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f,
+		-0.5f, 0.5f	
+	};
+		
+	float indexes[] {
+		0, 1, 2,
+		2, 3, 0 
+	};
+		
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 8 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	
+	unsigned int index;
+	glGenBuffers(1, &index);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * 2 * sizeof(float), indexes, GL_STATIC_DRAW);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
+    //~ /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        
+		/* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
+        
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+                
+		/* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
