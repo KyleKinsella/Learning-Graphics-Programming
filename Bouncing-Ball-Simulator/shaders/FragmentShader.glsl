07ff87dfd0049ed3f31e93cae1352a8ef92e55ec
KyleKinsella <t54420027@gmail.com>
2026-08-12 22:26:58 +0100

    i have created my own function that creates a circle. this is very good because now i can make as many circles as i want to (there is more work for this to be fully implemented)!

--- Bouncing-Ball-Simulator/shaders/FragmentShader.glsl
+++ Bouncing-Ball-Simulator/shaders/FragmentShader.glsl
@@ -2,17 +2,19 @@
 
 layout (location = 0) out vec4 color;
 
+#define UV gl_FragCoord.xy
+
+// TODO: add color...
+float createCircle(float lowerEdge, float higherEdge, float xCoord, float yCoord) {
+	return 1.0 - smoothstep(lowerEdge, higherEdge, distance(UV, vec2(xCoord, yCoord)));
+}
+
 void main() {
-	vec2 uv = gl_FragCoord.xy;
-	vec3 canvas = vec3(0.0);
-	
-	float circle = 1.0 - smoothstep(12.0, 50.0, distance(uv, vec2(245.0, 345.0)));
-	
-	float c1 = 1.0 - smoothstep(25.0, 80.0, distance(uv, vec2(120.0, 500.0))); // red
-	float c2 = 1.0 - smoothstep(2.0, 5.0, distance(uv, vec2(640.0, 320.0))); // purple
-	float c3 = 1.0 - smoothstep(20.0, 23.0, distance(uv, vec2(210.0, 80.0))); // pink
-	
-	canvas += vec3(1.0, 0.0, 0.0) * c1 + vec3(0.65, 0.56, 1.0) * c2 + vec3(1.0, 0.55, 0.76) * c3 + vec3(1.0, 1.0, 1.0) * circle;
-	
+	float whiteCircle = createCircle(12.0, 50.0, 245.0, 345.0);
+	float redCircle = createCircle(25.0, 80.0, 120.0, 500.0);
+	float purpleCircle = createCircle(2.0, 5.0, 640.0, 320.0);
+	float pinkCircle = createCircle(20.0, 23.0, 210.0, 80.0);
+		
+	vec3 canvas = vec3(1.0, 1.0, 1.0) * whiteCircle + vec3(1.0, 0.0, 0.0) * redCircle + vec3(0.65, 0.56, 1.0) * purpleCircle + vec3(1.0, 0.55, 0.76) * pinkCircle;
 	color = vec4(canvas, 1.0);
 }
