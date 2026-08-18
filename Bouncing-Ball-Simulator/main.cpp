62e4c17dd02a4d9426ac3513780c5e2e54d47055
KyleKinsella <t54420027@gmail.com>
2026-08-18 13:40:38 +0100

    i am working on making my ball(s) move!

--- Bouncing-Ball-Simulator/main.cpp
+++ Bouncing-Ball-Simulator/main.cpp
@@ -2,13 +2,26 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <vector>
 
 #include "Circle/Circle.h"
 #include "Shader/Shader.h"
 
-#define SCREEN_WIDTH 1066
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 1066 //3840
+#define SCREEN_HEIGHT 600 //2400
 #define NAME "Bouncing Ball Simulator"
+
+//~ void drawCircle(float centerX, float centerY, float radius, int res) {
+	//~ glBegin(GL_TRIANGLE_FAN);
+	//~ glVertex2d(centerX, centerY);
+	//~ for (int i = 0; i < res; ++i) {
+		//~ float angle = 2.0f * 3.141592653589 * (static_cast<float>(i) / res);
+		//~ float x = centerX + cos(angle) * radius;
+		//~ float y = centerY + sin(angle) * radius;
+		//~ glVertex2d(x, y);
+	//~ }
+	//~ glEnd();
+//~ }
 
 int main() {
     GLFWwindow* window;
@@ -63,11 +76,16 @@ int main() {
 	Shader shader("resources/shaders/VertexShader.glsl", "resources/shaders/FragmentShader.glsl");
 	shader.bind();
 	
+	//~ std::vector<float> pos = {400.0f, 300.0f};
+	
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
 		
 		/* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        //~ drawCircle(pos[0], pos[1], 50.0f, 50);
+        //~ pos[1] -= 1.0f;
         
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
                 
--- Bouncing-Ball-Simulator/shaders/FragmentShader.glsl
+++ Bouncing-Ball-Simulator/shaders/FragmentShader.glsl
@@ -18,16 +18,34 @@ float createCircle(float lowerEdge, float higherEdge, float xCoord, float yCoord
 	return 1.0 - smoothstep(lowerEdge, higherEdge, distance(UV, vec2(xCoord, yCoord)));
 }
 
-float createCircleAndBall(Ball ball) {
+float createCircle(Ball ball) {
 	return 1.0 - smoothstep(ball.lowerEdge, ball.higherEdge, distance(UV, vec2(ball.xCoord, ball.yCoord)));
+}
+
+float updateBallsYCoordinate(Ball ball, float newYValue) {
+	return ball.yCoord += newYValue;
+}
+
+float dropBall(float result, float end) {
+	//~ int i = 0;
+	//~ while (i != 20) {
+		//~ result -= 1.0;
+		//~ i++;
+	//~ }
+	
+	for (int i = 0; i < result - end; i++) {
+		result -= 1.0;
+	}
+	
+	return result;
 }
 
 void main() {
 	float whiteCircle = createCircle(12.0, 50.0, 245.0, 345.0);
 	float redCircle = createCircle(25.0, 80.0, 120.0, 500.0);
-	float purpleCircle = createCircle(2.0, 5.0, 640.0, 320.0);
+	float purpleCircle = createCircle(22.0, 44.0, 640.0, 320.0);
 	float pinkCircle = createCircle(20.0, 23.0, 210.0, 80.0);
-		
+	
 	vec3 canvas = vec3(1.0, 1.0, 1.0) * whiteCircle + vec3(1.0, 0.0, 0.0) * redCircle + vec3(0.65, 0.56, 1.0) * purpleCircle + vec3(1.0, 0.55, 0.76) * pinkCircle;
 	
 	Ball balls[BALLS];
@@ -37,12 +55,21 @@ void main() {
 		balls[i].xCoord = 100.0 + float(i) * 130.0;
 		balls[i].yCoord = 200.0;
 		balls[i].color = vec3(1.0, 0.0, 0.0);
+		
+		if (i == 4) {
+			balls[i].color = vec3(0.0, 1.0, 0.0);
+			
+			balls[i].yCoord += updateBallsYCoordinate(balls[i], 150.0);
+			
+			//~ balls[i].yCoord -= dropBall(balls[i].yCoord, -450.0); // at the top of the window (or very close to it)
+			balls[i].yCoord -= dropBall(balls[i].yCoord, 350.0); // very close to the bottom of the window
+		}
 	}
 	
 	for (int i = 0; i < BALLS; i++) {
-		float circle = createCircleAndBall(balls[i]);
+		float circle = createCircle(balls[i]);
 		canvas += circle * balls[i].color;
 	}
-	
+		
 	color = vec4(canvas, 1.0);
 }
