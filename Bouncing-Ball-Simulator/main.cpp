1152677529b32c03c1d5cbbbc6902f073370b141
KyleKinsella <t54420027@gmail.com>
2026-08-18 20:25:37 +0100

    tidy up!

--- Bouncing-Ball-Simulator/main.cpp
+++ Bouncing-Ball-Simulator/main.cpp
@@ -2,7 +2,6 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-//~ #include <vector>
 
 #include "Circle/Circle.h"
 #include "Shader/Shader.h"
@@ -10,45 +9,6 @@
 #define SCREEN_WIDTH 1066 //3840
 #define SCREEN_HEIGHT 600 //2400
 #define NAME "Bouncing Ball Simulator"
-
-
-//~ #include <cstdlib>
-//~ #include <ctime>
-
-
-//~ void drawCircle(float centerX, float centerY, float radius, int res) {
-	//~ glBegin(GL_TRIANGLE_FAN);
-	//~ glVertex2d(centerX, centerY);
-	//~ for (int i = 0; i < res; ++i) {
-		//~ float angle = 2.0f * 3.141592653589 * (static_cast<float>(i) / res);
-		//~ float x = centerX + cos(angle) * radius;
-		//~ float y = centerY + sin(angle) * radius;
-		//~ glVertex2d(x, y);
-	//~ }
-	//~ glEnd();
-//~ }
-
-//~ struct Ball {
-    //~ float x, y; // Position
-    //~ float velocityX, velocityY; // Velocity
-//~ };
-
-//~ Ball ball = {0.0f, 0.0f, 0.01f, 0.01f}; // Initial position and velocity
-
-
-//~ void updateBallPosition() {
-    //~ ball.x += ball.velocityX;
-    //~ ball.y += ball.velocityY;
-
-    //~ // Check for boundary collisions
-    //~ if (ball.x > 1.0f || ball.x < -1.0f) {
-        //~ ball.velocityX = -ball.velocityX; // Reverse direction
-    //~ }
-    //~ if (ball.y > 1.0f || ball.y < -1.0f) {
-        //~ ball.velocityY = -ball.velocityY; // Reverse direction
-    //~ }
-//~ }
-
 
 int main() {
     GLFWwindow* window;
@@ -103,58 +63,14 @@ int main() {
 	Shader shader("resources/shaders/VertexShader.glsl", "resources/shaders/FragmentShader.glsl");
 	shader.bind();
 	
-	
-	//~ float time = glfwGetTime();
-	//~ std::cout << time << std::endl;
-	
-	
-	//~ int loc = shader.getUniformName("time");
-	
-	//~ std::srand(std::time(0)); // Seed with current time
-	
-	
-	//~ if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		//~ std::cout << "the escape button was pressed!" << std::endl;
-	//~ }
-	
-	//~ std::vector<float> pos = {400.0f, 300.0f};
-	
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
 
 		float time = glfwGetTime();
 		std::cout << time << std::endl;
-
-	//~ if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		//~ std::cout << "the escape button was pressed!" << std::endl;
-	//~ }
-
-        //~ updateBallPosition();
-
 		
 		/* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        //~ drawCircle(pos[0], pos[1], 50.0f, 50);
-        //~ pos[1] -= 1.0f;
-        
-        //~ glPushMatrix();
-		
-		//~ glTranslatef(ball.x, ball.y, 0.0f); // Move the ball
-		
-		// Draw the ball (e.g., using glutSolidSphere)
-		//~ glPopMatrix();
-        
-        //~ updateBallPosition();
-        
-        //~ glPushMatrix();
-		//~ glTranslatef(ball.x, ball.y, 0.0f); // Move the ball
-		// Draw the ball (e.g., using glutSolidSphere)
-		//~ glPopMatrix();
-		
-		
-        //~ float randomFloat = static_cast<float>(std::rand()) / RAND_MAX; // Random float between 0.0 and 1.0
-        //~ shader.createNBalls("time", randomFloat);
         
         shader.bind();
         shader.createNBalls("time", time);
