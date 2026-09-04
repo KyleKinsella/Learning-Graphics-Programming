b6875b617a266af5dbd7227ab62c0dcf6b30e7e6
KyleKinsella <t54420027@gmail.com>
2026-09-04 23:32:37 +0100

    the two functions called 'updateBallsYCoordinate' and 'dropBall' i removed these functions from my fragment shader to my circle source file, this is where these are supposed to be!

--- Bouncing-Ball-Simulator/Circle/Circle.cpp
+++ Bouncing-Ball-Simulator/Circle/Circle.cpp
@@ -1,7 +1,27 @@
 #include "Circle.h"
 
-Circle::Circle(int origin, int radius, int velocity, glm::vec3 color) : m_origin(origin), m_radius(radius), m_velocity(velocity), m_color(color) {
+Circle::Circle(float lowerEdge, float higherEdge, float xCoord, float yCoord, glm::vec3 color) {
+	m_lowerEdge = lowerEdge;
+	m_higherEdge = higherEdge;
+	m_xCoord = xCoord;
+	m_yCoord = yCoord;
+	m_color = color;
 }
 
 Circle::~Circle() {
+}
+
+float Circle::updateBallsYCoordinate(Circle circle, float newYValue, float timeElapsed) {
+	// a ball is zero, so give him a value so he can move
+	if (newYValue == 0) 
+		newYValue = 2.0;
+			
+	return circle.m_yCoord += sin(timeElapsed * newYValue) * 100.0;
+}
+
+float Circle::dropBall(float result, float end) {
+	for (int i = 0; i < result - end; i++) {
+		result -= 1.0;
+	}	
+	return result;
 }
