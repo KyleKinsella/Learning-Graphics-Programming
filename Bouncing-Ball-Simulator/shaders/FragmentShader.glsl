91eafdea52406714c106657563f1ab412dab2fbb
KyleKinsella <t54420027@gmail.com>
2026-09-04 23:38:05 +0100

    i have removed two of my functions out of this shader and now into my circle.h and circle.cpp file, this has actually improved the processing speed of my gpu, but there is more work for my cpu to do... i have wrote a new function, this is called: 'createObject(...)' this is going to be used for either creating balls(s) or star(s) - for my ray tracing project...

--- Bouncing-Ball-Simulator/shaders/FragmentShader.glsl
+++ Bouncing-Ball-Simulator/shaders/FragmentShader.glsl
@@ -1,10 +1,11 @@
 #version 330 core
 
-layout (location = 0) out vec4 fragColor;
+out vec4 fragColor;
 
 #define UV gl_FragCoord.xy
 #define BALLS 5
 
+// making a ball
 uniform float time;
 uniform float lowerEdge;
 uniform float higherEdge;
@@ -12,19 +13,26 @@ uniform float xCoord;
 uniform float yCoord;
 uniform vec3 color;
 
-// these are for updating a ball
+// updating a ball
 uniform float updateLowerEdge;
 uniform float updateHigherEdge;
 uniform float updateXCoord;
 uniform float updateYCoord;
 uniform vec3 updateColor;
 
+// texture stuff
 uniform sampler2D u_Texture;
 in vec2 u_TexCoord;
 
+// what ball am i updating ?
 uniform int BALL_TO_UPDATE;
 
+// determines the output color for each fragment / pixel
 uniform bool u_bool;
+
+// moving the ball(s)
+uniform float u_newYVal;
+uniform float u_result;
 
 struct Ball {
 	float lowerEdge, higherEdge, xCoord, yCoord;
@@ -39,20 +47,14 @@ float createCircle(Ball ball) {
 	return 1.0 - smoothstep(ball.lowerEdge, ball.higherEdge, distance(UV, vec2(ball.xCoord, ball.yCoord)));
 }
 
-float updateBallsYCoordinate(Ball ball, float newYValue, float timeElapsed) {
-	// a ball is zero, so give him a value so he can move
-	if (newYValue == 0) 
-		newYValue = 2.0;
-	
-	return ball.yCoord += sin(timeElapsed * newYValue) * 100.0;
-}
-
-float dropBall(float result, float end) {
-	for (int i = 0; i < result - end; i++) {
-		result -= 1.0;
+void createObject(bool object) {
+	if (object) {
+		// create one or many balls
+		fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	} else {
+		// create one or many stars (for my Ray Tracing)...
+		fragColor = vec4(0.0, 0.0, 1.0, 1.0);
 	}
-	
-	return result;
 }
 
 void main() {
@@ -63,7 +65,7 @@ void main() {
 	
 	float guiCircle = createCircle(lowerEdge, higherEdge, xCoord, yCoord);
 	vec3 canvas = vec3(1.0, 1.0, 1.0) * whiteCircle + vec3(1.0, 0.0, 0.0) * redCircle + vec3(0.65, 0.56, 1.0) * purpleCircle + vec3(1.0, 0.55, 0.76) * pinkCircle + color * guiCircle;
-		
+	
 	Ball balls[BALLS];
 	for (int i = 0; i < BALLS; i++) {
 		balls[i].lowerEdge = 12.0;
@@ -74,8 +76,8 @@ void main() {
 		
 		if (i == BALLS-1) {
 			balls[i].color = vec3(0.0, 1.0, 0.0);
-			balls[i].yCoord += updateBallsYCoordinate(balls[i], i*2.0, time);
-			balls[i].yCoord -= dropBall(balls[i].yCoord, 130.0);
+			balls[i].yCoord += u_newYVal;
+			balls[i].yCoord -= u_result;
 		}
 		
 		// this poses a kinda big issue/problem...
@@ -88,36 +90,21 @@ void main() {
 		balls[BALL_TO_UPDATE].xCoord = updateXCoord;
 		balls[BALL_TO_UPDATE].yCoord = updateYCoord;
 		balls[BALL_TO_UPDATE].color = updateColor;
-		
-		balls[BALL_TO_UPDATE].yCoord += updateBallsYCoordinate(balls[BALL_TO_UPDATE], i*2.0, time);
-		balls[BALL_TO_UPDATE].yCoord -= dropBall(balls[BALL_TO_UPDATE].yCoord, 130.0);
+			
+		balls[BALL_TO_UPDATE].yCoord += u_newYVal;
+		balls[BALL_TO_UPDATE].yCoord -= u_result;
 	}
-	
+		
 	for (int i = 0; i < BALLS; i++) {
 		float circle = createCircle(balls[i]);		
 		canvas += circle * balls[i].color;
 	}
-	
-	//~ fragColor = vec4(canvas, 1.0);
-	
+			
 	if (u_bool) {
 		fragColor = vec4(canvas, 1.0);
+		//~ createObject(false);
 	} else {
 		vec4 textCoord = texture(u_Texture, u_TexCoord);
-		//~ fragColor = textCoord;
 		fragColor = guiCircle * textCoord;
 	}
-	
-	//~ if (u_bool == 2) {
-		//~ fragColor = vec4(1.0, 0.0, 0.0, 1.0);
-	//~ }
-	
-	//~ vec4 textCoord = texture(u_Texture, u_TexCoord);
-	//~ fragColor = textCoord;
-	//~ fragColor = guiCircle * textCoord;
-		
-	//~ float test = createCircle(balls[BALLS-1].lowerEdge, balls[BALLS-1].higherEdge, balls[BALLS-1].xCoord, balls[BALLS-1].yCoord);
-	//~ fragColor = test * textCoord;
-	
-	//~ fragColor = vec4(UV.x / 1066.0, UV.y / 600, 0.0, 1.0);
 }
