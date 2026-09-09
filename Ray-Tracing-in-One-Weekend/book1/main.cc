4705f45f5b301c253fa20314b2bea6f0448a44d9
KyleKinsella <t54420027@gmail.com>
2026-09-09 20:35:30 +0100

    i forgot to commit this earlier today, after we got the red sphere showing we then did this to show the same sphere but, the sphere has color's according to its normal vectors

--- Ray-Tracing-in-One-Weekend/book1/main.cc
+++ Ray-Tracing-in-One-Weekend/book1/main.cc
@@ -3,15 +3,41 @@
 #include "hittable_list.h"
 #include "sphere.h"
 
-color ray_color(const ray& r, const hittable& world) {
-	hit_record rec;
-	if (world.hit(r, 0, infinity, rec)) {
-		return 0.5 * (rec.normal + color(1,1,1));
-	}
+//~ color ray_color(const ray& r, const hittable& world) {
+	//~ hit_record rec;
+	//~ if (world.hit(r, 0, infinity, rec)) {
+		//~ return 0.5 * (rec.normal + color(1,1,1));
+	//~ }
 		
-	vec3 unit_direction = unit_vector(r.direction());
-	auto a = 0.5*(unit_direction.y() + 1.0);
-	return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+	//~ vec3 unit_direction = unit_vector(r.direction());
+	//~ auto a = 0.5*(unit_direction.y() + 1.0);
+	//~ return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+//~ }
+
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - std::sqrt(discriminant) ) / (2.0*a);
+    }
+}
+
+color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -24,9 +50,9 @@ int main() {
 	image_height = (image_height < 1) ? 1 : image_height;
 	
 	// world
-	hittable_list world;
-	world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-	world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+	//~ hittable_list world;
+	//~ world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+	//~ world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 	
 	// camera
 	auto focal_length = 1.0;
@@ -56,7 +82,8 @@ int main() {
 			auto ray_direction = pixel_center - camera_center;
 			ray r(camera_center, ray_direction);
 			
-			color pixel_color = ray_color(r, world);
+			color pixel_color = ray_color(r);
+			//~ color pixel_color = ray_color(r, world);
 			write_color(std::cout, pixel_color);
 		}
 	}
