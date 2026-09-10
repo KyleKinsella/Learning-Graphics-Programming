1f5110c31ab8c784bb9aa14d50b9d1fda27c9725
KyleKinsella <t54420027@gmail.com>
2026-09-10 15:14:41 +0100

    i have now adding diffuse materials

--- Ray-Tracing-in-One-Weekend/book1/rtweekend.h
+++ Ray-Tracing-in-One-Weekend/book1/rtweekend.h
@@ -2,9 +2,11 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 // c++ std usings
 using std::make_shared;
@@ -17,6 +19,19 @@ const double pi = 3.1425926535897932385;
 // utility functions
 inline double degrees_to_radians(double degrees) {
 	return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+	
+	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	static std::mt19937 generator;
+	return distribution(generator);
+	
+	//~ return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+	return min  + (max - min) * random_double();
 }
 
 // common headers
