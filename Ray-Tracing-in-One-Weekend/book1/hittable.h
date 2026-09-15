171badb5e14d2d3c796c4787ffef0f4a6674602a
KyleKinsella <t54420027@gmail.com>
2026-09-15 14:58:23 +0100

    i have now added two brand new data structures: Bounding Volume Hierarchies (BVH's) and Axis-Aligned Bounding Boxes (AABB's)

--- Ray-Tracing-in-One-Weekend/book1/hittable.h
+++ Ray-Tracing-in-One-Weekend/book1/hittable.h
@@ -2,6 +2,7 @@
 #define HITTABLE_H
 
 #include "rtweekend.h"
+#include "../book2/aabb.h"
 
 class material;
 
@@ -26,6 +27,7 @@ class hittable {
 public:
 	virtual ~hittable() = default;	
 	virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+	virtual aabb bounding_box() const = 0;
 };
 
 #endif
