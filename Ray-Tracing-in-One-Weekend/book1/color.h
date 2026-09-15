8cfe04758b619d10c7e703f67f2624d67245c333
KyleKinsella <t54420027@gmail.com>
2026-09-15 12:51:45 +0100

    today i have started the second ray tracing book! i have done some small changes here and there. the code up to this point runs very very slowly, the next thing to do is to make our code faster

--- Ray-Tracing-in-One-Weekend/book1/color.h
+++ Ray-Tracing-in-One-Weekend/book1/color.h
@@ -15,9 +15,9 @@ inline double linear_to_gamma(double linear_component) {
 }
 
 void write_color(std::ostream& out, const color& pixel_color) {
-	auto r  = pixel_color.x();
-	auto g  = pixel_color.y();
-	auto b  = pixel_color.z();
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
 	
 	// apply a linear to gamma transform for gamma 2
 	r = linear_to_gamma(r);
--- Ray-Tracing-in-One-Weekend/book1/material.h
+++ Ray-Tracing-in-One-Weekend/book1/material.h
@@ -16,13 +16,13 @@ class lambertian : public material {
 public:
 	lambertian(const color& albedo) : albedo(albedo) {}
 
-	bool scatter(const ray& r, const hit_record& rec, color& attenuation, ray& scattered) const override {
+	bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
 		auto scatter_direction = rec.normal + random_unit_vector();
 		
 		if (scatter_direction.near_zero())
 			scatter_direction = rec.normal;
 		
-		scattered = ray(rec.p, scatter_direction);
+		scattered = ray(rec.p, scatter_direction, r_in.time());
 		attenuation = albedo;
 		return true;
 	}
@@ -39,7 +39,7 @@ public:
 		vec3 reflected = reflect(r_in.direction(), rec.normal);
 		reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
 		
-		scattered = ray(rec.p, reflected);
+		scattered = ray(rec.p, reflected, r_in.time());
 		attenuation = albedo;
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
@@ -72,7 +72,7 @@ public:
 			direction = refract(unit_direction, rec.normal, ri);
 		}
 		
-		scattered = ray(rec.p, direction);
+		scattered = ray(rec.p, direction, r_in.time());
 		return true;
 	}
 
