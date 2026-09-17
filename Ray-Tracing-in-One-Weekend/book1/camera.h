24641d9dedc9a3a7c11a4123408faf0414805442
KyleKinsella <t54420027@gmail.com>
2026-09-17 21:53:52 +0100

    i have now added lights, this is very very cool!

--- Ray-Tracing-in-One-Weekend/book1/camera.h
+++ Ray-Tracing-in-One-Weekend/book1/camera.h
@@ -10,6 +10,7 @@ public:
 	int image_width = 100;
 	int samples_per_pixels = 10;
 	int max_depth = 10;
+	color background; // scene background color
 	
 	// vertical view angle - field of view (aka, fov)
 	double vfov = 90;
@@ -117,18 +118,24 @@ private:
 		}
 		
 		hit_record rec;
-		if (world.hit(r, interval(0.001, infinity), rec)) {
-			ray scattered;
-			color attenuation;
-			if (rec.mat->scatter(r, rec, attenuation, scattered))
-				return attenuation * ray_color(scattered, depth-1, world);
-				
-			return color(0, 0, 0);
-		}
+		
+		// if the ray hits nothing, return the background color
+		if (!world.hit(r, interval(0.001, infinity), rec))
+			return background;
+		
+		ray scattered;
+		color attenuation;
+		color color_from_emission = rec.mat->emitted(rec.u, rec.v, rec.p);
+		
+		if (!rec.mat->scatter(r, rec, attenuation, scattered))
+			return color_from_emission;
 			
-		vec3 unit_direction = unit_vector(r.direction());
-		auto a = 0.5*(unit_direction.y() + 1.0);
-		return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+		color color_from_scatter = attenuation * ray_color(scattered, depth-1, world);
+		return color_from_emission + color_from_scatter;
+			
+		//~ vec3 unit_direction = unit_vector(r.direction());
+		//~ auto a = 0.5*(unit_direction.y() + 1.0);
+		//~ return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 	}
 };
 
